f6b1f63a5c01dd6cfe30cf9e932d948955e6d13e
Rogue-source <TheMustardLog@gmail.com>
2026-02-01 00:51:10 +0200

    Update Cleanup.cs

--- Editor/Cleanup.cs
+++ Editor/Cleanup.cs
@@ -123,7 +123,7 @@ public static class Cleanup
             "Assets/MineMogul/Game/Scripts/Unity.VisualScripting.Flow",
             "Assets/MineMogul/Game/Scripts/Unity.VisualScripting.State",
 			"Assets/MineMogul/Game/Scripts/Unity.Collections.LowLevel",
-			"Assets/MineMogul/Game/Scripts/Unity.Timeline"
+			"Assets/MineMogul/Game/Scripts/Unity.Timeline",
             
             // DOTween & DLL Overlaps
             "Assets/MineMogul/Game/Scripts/DemiLib",
